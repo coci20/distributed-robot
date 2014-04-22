44d5efcf4cb1d96ad4c6b8b9b228b1fc9b2a026c
Prasannjit Kumar <pk435@cornell.edu>
2014-04-22 07:10:13 -0400

    updated code

--- Main.cs
+++ Main.cs
@@ -11,26 +11,17 @@ using System.Text;
 
 namespace ConsoleApp1
 {
+	/// <summary>
+	/// Main class.
+	/// </summary>
 	class MainClass
 	{
 		public static void Main (string[] args)
 		{
-			//populate points in a File named "input1.txt"
 
 			bool render=true;
-			//double incV=0.0005f;
-			Points pt=new Points();
 
-			Console.WriteLine("created a point");
-
-			Console.WriteLine("create a list of obstacle points");
-
-			List<Points> obstacleList=new List<Points>();
-			List<Points> pointList = populatePoints("input1.txt");
-			int size = pointList.Count;
-
-			fillObstaclePoints(obstacleList,size,pointList);
-			Thread.Sleep (20);
+			MazeInstance maze1 = new MazeInstance ();
 
 			using (var game = new GameWindow())
             {
@@ -66,7 +57,7 @@ namespace ConsoleApp1
  					GL.PointSize(4.9f);
                     
 					//show the obstacles on the grid
-					foreach(Points p in obstacleList){
+					foreach(Points p in maze1.get_list_of_Obstacles()){
 						GL.Begin(PrimitiveType.Points);
 						GL.Color3(Color.Yellow);
 						GL.Vertex2(p.getX(),p.getY());
@@ -74,84 +65,26 @@ namespace ConsoleApp1
 						game.SwapBuffers();
 					}
 					//move across the grind handling obstacles
-					foreach(Points p in pointList){
+					foreach(Points p in maze1.get_list_of_Points()){
 						GL.Begin(PrimitiveType.Points);
-						moveAcrossGrid(p,obstacleList);
+						moveAcrossGrid(p);
 						GL.End();
 						game.SwapBuffers();
 					}
 
-					//single obstacle
-                    //GL.Color3(Color.MidnightBlue);
-					//GL.Vertex2(0.5f,0.5f);
-					//for(double x=-0.8;x<=0.8;x+=0.1){
-					//	for(double y=0.8;y>=-0.8;y-=0.1){
-					//create movement across the grid
-					//present obstacles
-
-//					if(pt.getX() <0.4f || pt.getX() >0.6){
-//								GL.Color3(Color.DarkSeaGreen);
-//								GL.Vertex2(pt.getX(),pt.getY());
-//								Console.WriteLine(pt.toString());
-//					}
-					//	}
-					//}
 					render=false;
 
-					//pt.incPoint(incV);
-                    
-					//Thread.Sleep(15);
                 };
                 // Run the game at 60 updates per second
 				game.Run (60.0);
             }
 		}
 
-		static void fillObstaclePoints (List<Points> obstacleList,
-		                                int size,
-		                                List<Points> pointList)
-		{
-			//hard-coding the list of obstacles
-			//define here the #obstacles
-			int no_obstacles = 100;
-			Random rand = new Random ();
-			for (int i=0; i<100; i++) {
-				int randIndex = rand.Next (0, size);
-				Points pt = pointList [randIndex];
-				pt.setAsObstacle ();
-				obstacleList.Add (pt);
-			}
-			Console.WriteLine (obstacleList.Count);
-
-		}
-		static bool aroundObstacle (Points pt, List<Points> obstacleList)
-		{
-			//create obstacle points
-			//bool check=false;
-			double range=0.05f;
-			double currX=pt.getX();
-			double currY=pt.getY();
-			foreach (Points p in obstacleList) {
-				double obsX=p.getX();
-				double obsY=p.getY();
-
-				//Console.WriteLine(p.toString());
-				if((currX<obsX-range || currX>obsX+range)
-					&&(currY<obsY-range||currY>obsY+range)){
-					;//not an obstacle, do nothing
-				}
-				else{
-					GL.Color3(Color.Aqua);
-					GL.Vertex2(pt.getX(),pt.getY());
-					//Console.WriteLine(pt.toString());
-					return true;
-				}
-			}
-			return false;
-			//throw new NotImplementedException ();
-		}
-
-		static void moveAcrossGrid (Points pt,List<Points> obstacleList)
+		/// <summary>
+		/// Moves the point across the grid.
+		/// </summary>
+		/// <param name="pt">Point.</param>
+		static void moveAcrossGrid (Points pt)
 		{
 			//start movement
 			//if not around an obstacle then draw the point
@@ -161,53 +94,7 @@ namespace ConsoleApp1
 				GL.Vertex2(pt.getX(),pt.getY());
 			}
 
-
 		}	
-
-
-		static List<Points> populatePoints (string inputtxt)
-		{
-			using (StreamWriter F = new StreamWriter("input1.txt")) { 
-				for (double x=-0.8f; x<=0.8f; x+=0.05) {
-					for (double y=-0.8f; y<=0.8f; y+=0.05) {
-						Points p = new Points ();
-						p.putPoint (x, y);
-						//byte bArray=convertToByte(p.toString());
-						F.WriteLine (p.toString ());
-					}
-				}
-			}
-			List<Points> myList = listPoints(inputtxt);
-			return myList;
-		}
-			//F.Close();		
-		static List<Points> listPoints (string inputtxt)
-		{
-			//int x = 0;
-			List<Points> myList = new List<Points>();
-				using (StreamReader reader= new StreamReader(inputtxt)) {
-					string line;
-					while ((line=reader.ReadLine())!=null) {
-			//			Console.WriteLine (x + " " + line);
-			//			x++;
-					string[] delim={","};
-					string[] s=line.Split(delim,StringSplitOptions.RemoveEmptyEntries);
-					double X=Convert.ToDouble(s[0]);
-					double Y=Convert.ToDouble(s[1]);
-					Points p=new Points();
-					p.putPoint (X,Y);
-					myList.Add(p);
-					}
-				}
-
-	//List<Points> myList = listPoints(inputtxt);
-			return myList;
-			}
-
-
-
-		
-
 
 	}
 }
