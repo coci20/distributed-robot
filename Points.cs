227e76bd0fa3d6ee6326eec874f122d852761adf
Prasannjit Kumar <pk435@cornell.edu>
2014-04-22 07:10:55 -0400

    updated

--- Points.cs
+++ Points.cs
@@ -15,22 +15,29 @@ namespace ConsoleApp1
 			isObstacle = false;
 		}
 
-		public void incPoint (double incV)
-		{
-			x += incV;
-			y += incV;
-		}
-
+		/// <summary>
+		/// Gets the x.
+		/// </summary>
+		/// <returns>The x.</returns>
 		public double getX ()
 		{
 			return x;
 		}
 
+		/// <summary>
+		/// Gets the y.
+		/// </summary>
+		/// <returns>The y.</returns>
 		public double getY ()
 		{	
 			return y;
 		}
 
+		/// <summary>
+		/// Puts the point.
+		/// </summary>
+		/// <param name="s1">S1.</param>
+		/// <param name="s2">S2.</param>
 		public void putPoint (double s1, double s2)
 		{
 			x=s1;
@@ -42,10 +49,17 @@ namespace ConsoleApp1
 			return x + "," + y;
 		}
 
+		/// <summary>
+		/// Sets as obstacle.
+		/// </summary>
 		public void setAsObstacle(){
 			this.isObstacle=true;
 		}
 
+		/// <summary>
+		/// Checks the obstacle point.
+		/// </summary>
+		/// <returns><c>true</c>, if obstacle point, <c>false</c> otherwise.</returns>
 		public bool isObstaclePoint(){
 			return isObstacle;
 		}
